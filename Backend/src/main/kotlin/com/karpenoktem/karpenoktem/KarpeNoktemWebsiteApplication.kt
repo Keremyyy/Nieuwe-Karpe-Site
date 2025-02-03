96280794f75a5615203827d1f1c852632bf9454b
Marijn Hazeveld <34924077+Keremyyy@users.noreply.github.com>
2025-02-03 21:12:05 +0100

    add

---
+++ Backend/src/main/kotlin/com/karpenoktem/karpenoktem/KarpeNoktemWebsiteApplication.kt
@@ -0,0 +1,11 @@
+package com.karpenoktem.karpenoktem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KarpeNoktemWebsiteApplication
+
+fun main(args: Array<String>) {
+	runApplication<KarpeNoktemWebsiteApplication>(*args)
+}
---
+++ Backend/src/test/kotlin/com/karpenoktem/karpenoktem/KarpeNoktemWebsiteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.karpenoktem.karpenoktem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KarpeNoktemWebsiteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
